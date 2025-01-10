73a55f4a3978268fe5b41d1a3ea0e6d91d738d1b
Unnati_v_kohale <kohaleunnati@gmail.com>
2025-01-10 23:03:04 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -8,29 +8,75 @@ terraform {
 }
 provider "aws" {
   region = "ap-southeast-2"
-  
 }
-resource "aws_instance" "myserver"{
- ami = var.image_id
- instance_type =  var.instance_type
- key_name = var.key_name
- security_groups = var.security_groups
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "terra_vpc"
+  }
 }
-variable "image_id"{
-  default = "ami-003f5a76758516d1e"
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pub_subnet"
+  }
 }
-variable "instance_type" {
-  default = "t2.micro"
-  
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-2b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "pri_subnet"
+  }
 }
-variable "key_name" {
-  default = "tirthraj-sydkey"
-  
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
 }
-variable "security_groups" {
-  default = ["default"]
-  
+
+resource "aws_route_table" "pub_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "pub_route_table"
+  }
 }
-  
+
+resource "aws_route_table_association" "pub_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.pub_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "pri_route_table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
