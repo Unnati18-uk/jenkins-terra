401e24c3beba5e9f68e54df738226b371e4a13b9
Unnati_v_kohale <kohaleunnati@gmail.com>
2025-01-10 19:33:51 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -71,3 +71,5 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
+}
+
